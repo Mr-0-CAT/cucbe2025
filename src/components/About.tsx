c5c2345f59279e867b37c6557aed2c16459f52a8
Mr-0-CAT <155630072+Mr-0-CAT@users.noreply.github.com>
2025-07-17 14:09:22 +0530

    .

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,8 @@ const About = () => {
       <div className="row justify-content-center">
         <div className="col-lg-10">
           <div className="modern-card fade-in-left">
-            <h3 style={{fontSize: '28px', color: '#004080'}}>About the Istitution</h3>
+            <h3 style={{fontSize: '28px', color: '#004080'}}>About the Institution
+            </h3>
             <p style={{fontSize: '18px', lineHeight: '1.8'}}>
               RNS Institute of Technology, familiar as RNSIT was established in the year 2001. It is the brainchild of Dr. R. N. Shetty, well known industrialist, and a great philanthropist. Lush green campus, state of the art infrastructure, well equipped laboratories, hundreds of activities, numerous new initiatives, consistent VTU ranks, path breaking placements, cultural extravaganza, Sports events, 6000+ students and 300+ faculty – with all these RNSIT has become dream destination for engineering aspirants across the country. With 9 UG Engineering branches, 2 PG courses and 8 R&D Centers RNSIT is aiming towards becoming the premier engineering institutes in the country
             </p>
